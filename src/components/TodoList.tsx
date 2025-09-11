2fe81b25b431d810c2549703e71783fe5c404f40
biniyam yoseph <biniyamyoseph063@gmail.com>
2025-09-11 21:17:59 +0300

    scroller

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { ScrollArea } from "./ui/scroll-area";
+import { Card } from "./ui/card";
+import { Checkbox } from "./ui/checkbox";
+
+const TodoList = () => {
+  return (
+    <div className="">
+      Calander
+      {/* {List} */}
+      <ScrollArea className="max-h-[400px] mt-4 overflow-y-auto ">
+        <div className="flex flex-col gap-4">
+          {/* List item */}
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox className="item1" />
+              <label htmlFor="item1" className="text-sm text-muted-foreground">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </label>
+            </div>
+          </Card>
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox className="item1" />
+              <label htmlFor="item1" className="text-sm text-muted-foreground">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </label>
+            </div>
+          </Card>
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox className="item1" />
+              <label htmlFor="item1" className="text-sm text-muted-foreground">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </label>
+            </div>
+          </Card>
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox className="item1" />
+              <label htmlFor="item1" className="text-sm text-muted-foreground">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </label>
+            </div>
+          </Card>
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox className="item1" />
+              <label htmlFor="item1" className="text-sm text-muted-foreground">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </label>
+            </div>
+          </Card>
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox className="item1" />
+              <label htmlFor="item1" className="text-sm text-muted-foreground">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </label>
+            </div>
+          </Card>
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox className="item1" />
+              <label htmlFor="item1" className="text-sm text-muted-foreground">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </label>
+            </div>
+          </Card>
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox className="item1" />
+              <label htmlFor="item1" className="text-sm text-muted-foreground">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </label>
+            </div>
+          </Card>
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox className="item1" />
+              <label htmlFor="item1" className="text-sm text-muted-foreground">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </label>
+            </div>
+          </Card>
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox className="item1" />
+              <label htmlFor="item1" className="text-sm text-muted-foreground">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              </label>
+            </div>
+          </Card>
+        </div>
+      </ScrollArea>
+    </div>
+  );
+};
+
+export default TodoList;
