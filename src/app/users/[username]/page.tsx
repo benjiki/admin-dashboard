915fe42b9427015f239fbe997c57c2735dfbd6ba
biniyam yoseph <biniyamyoseph063@gmail.com>
2025-09-11 22:19:01 +0300

    users right side containers

--- src/app/users/[username]/page.tsx
+++ src/app/users/[username]/page.tsx
@@ -1,7 +1,49 @@
 import React from "react";
-
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import CardList from "@/components/CardList";
 const SingleUserPage = () => {
-  return <div>SingleUserPage</div>;
+  return (
+    <div>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">DashBoard</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/users">Users</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      {/* Container */}
+      <div className="mt-4 flex flex-col xl:flex-row gap-8">
+        {/* Left */}
+        <div className="w-full xl:w-1/3 space-y-6">
+          {/* user badges container */}
+          <div className="bg-primary-foreground p-4 rounded-lg"></div>
+          {/* information container */}
+          <div className="bg-primary-foreground p-4 rounded-lg"></div>
+          {/* Cart List container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <CardList title="Recently Transactions" />
+          </div>
+        </div>
+        {/* Right */}
+        <div className="w-full xl:w-2/3 space-y-6"></div>
+      </div>
+    </div>
+  );
 };
 
 export default SingleUserPage;
