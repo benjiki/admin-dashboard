a2503cb80626d7c2ca61c447af7883df81d19264
biniyam yoseph <biniyamyoseph063@gmail.com>
2025-09-12 19:13:40 +0300

    data table pagination

--- src/app/payment/data-table.tsx
+++ src/app/payment/data-table.tsx
@@ -17,6 +17,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
+import { DataTablePagination } from "@/components/TablePagination";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -86,7 +87,7 @@ export function DataTable<TData, TValue>({
           </TableBody>
         </Table>
       </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
+      {/* <div className="flex items-center justify-end space-x-2 py-4">
         <Button
           variant="outline"
           size="sm"
@@ -103,7 +104,8 @@ export function DataTable<TData, TValue>({
         >
           Next
         </Button>
-      </div>
+      </div> */}
+      <DataTablePagination table={table} />
     </div>
   );
 }
